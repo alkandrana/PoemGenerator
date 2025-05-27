6d815f0311e91f6c8234c0fb8d8320d0532962d7
Rosa Myers <larosaardente@gmail.com>
2025-05-27 13:02:17 -0700

    Setup webpack.

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const htmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'poemgenerator.bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', {targets: "defaults"}]
+                        ]
+                    }
+                }
+            },
+        ],
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: "./index.html"
+        })
+    ],
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    optimization: {
+        runtimeChunk: 'single',
+    },
+
+};
